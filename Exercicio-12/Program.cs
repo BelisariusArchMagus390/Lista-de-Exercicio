f2c52b861ec68f8bf02bcc12745eae82e418a8ea
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-04-08 17:12:04 -0300

    Add and completed Exercicio-12

---
+++ Exercicio-12/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercicio_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ---------------------------------------------");
+            Console.WriteLine($"\n NÚMEROS ÍMPARES ENTRE 100 E 200");
+            Console.WriteLine("\n ---------------------------------------------");
+            showOddNumbers();
+            Console.WriteLine(" ---------------------------------------");
+            Console.WriteLine("\n Aperte Enter para sair...");
+            Console.ReadLine();
+        }
+
+        static void showOddNumbers()
+        {
+            for (int number = 100; number <= 200; number++)
+            {
+                if (number % 2 != 0)
+                    Console.WriteLine($" {number}");
+            }
+        }
+    }
+}
